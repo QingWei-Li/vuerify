0432411a57b8fb85f1bbbd2b13474c5d10b15bef
kokoro <8462304@qq.com>
2016-09-27 16:33:54 +0800

    修复Object.assign不兼容android4.0原生浏览器的问题

--- dist/vuerify.js
+++ dist/vuerify.js
@@ -2,121 +2,145 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global.Vuerify = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
-  var RULES = {
-    email: {
-      test: /^(([^<>()[\]\\.,;:\s"]+(\.[^<>()[\]\\.,;:\s"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      message: '邮箱格式错误'
-    },
-    required: {
-      test: /\S+$/,
-      message: '必填项'
-    },
-    url: {
-      test: /^(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i,
-      message: 'URL 格式错误'
-    }
-  };
+var RULES = {
+  email: {
+    test: /^(([^<>()[\]\\.,;:\s"]+(\.[^<>()[\]\\.,;:\s"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+    message: '邮箱格式错误'
+  },
+  required: {
+    test: /\S+$/,
+    message: '必填项'
+  },
+  url: {
+    test: /^(https?|ftp|rmtp|mms):\/\/(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)(:(\d+))?\/?/i,
+    message: 'URL 格式错误'
+  }
+};
 
-  var _toString = Object.prototype.toString;
-  var Vue$1 = void 0;
+var _toString = Object.prototype.toString;
+var Vue$1 = void 0;
 
-  function check(field, value) {
-    var $rules = this.$vuerify.$rules;
-    var fields = this.$options.vuerify;
-    var rule = fields[field];
-    var regex = _toString.call(rule) === '[object String]' ? $rules[rule] : _toString.call(rule.test) === '[object String]' ? $rules[rule.test] : rule;
+function check(field, value) {
+  var $rules = this.$vuerify.$rules;
+  var fields = this.$options.vuerify;
+  var rule = fields[field];
+  var regex = _toString.call(rule) === '[object String]' ? $rules[rule] : _toString.call(rule.test) === '[object String]' ? $rules[rule.test] : rule;
 
-    if (!regex || !regex.test) {
-      console.warn('[vuerify] rule does not exist: ' + (rule.test || rule));
-      return;
-    }
-    regex.message = rule.message || regex.message;
+  if (!regex || !regex.test) {
+    console.warn('[vuerify] rule does not exist: ' + (rule.test || rule));
+    return;
+  }
+  regex.message = rule.message || regex.message;
 
-    var oldError = this.$vuerify.$errors[field];
-    var valid = _toString.call(regex.test) === '[object Function]' ? regex.test.call(this, value) : regex.test.test(value);
+  var oldError = this.$vuerify.$errors[field];
+  var valid = _toString.call(regex.test) === '[object Function]' ? regex.test.call(this, value) : regex.test.test(value);
 
-    if (valid) {
-      Vue$1.delete(this.$vuerify.$errors, field);
-    } else if (!oldError) {
-      Vue$1.set(this.$vuerify.$errors, field, regex.message);
-    }
-
-    var hasError = Boolean(Object.keys(this.$vuerify.$errors).length);
-
-    this.$vuerify.valid = !hasError;
-    this.$vuerify.invalid = hasError;
-
-    return valid;
+  if (valid) {
+    Vue$1.delete(this.$vuerify.$errors, field);
+  } else if (!oldError) {
+    Vue$1.set(this.$vuerify.$errors, field, regex.message);
   }
 
-  function checkAll(fields) {
-    var vm = this.vm;
+  var hasError = Boolean(Object.keys(this.$vuerify.$errors).length);
 
-    fields = fields || Object.keys(vm.$options.vuerify);
+  this.$vuerify.valid = !hasError;
+  this.$vuerify.invalid = hasError;
 
-    return fields.map(function (field) {
-      return check.call(vm, field, vm._data[field]);
-    }).indexOf(false) === -1;
-  }
+  return valid;
+}
 
-  var Vuerify = function Vuerify(_vm) {
-    this.vm = _vm;
-  };
+function checkAll(fields) {
+  var vm = this.vm;
 
-  Vuerify.prototype.check = function (fields) {
-    return checkAll.call(this, fields);
-  };
+  fields = fields || Object.keys(vm.$options.vuerify);
 
-  Vuerify.prototype.clear = function () {
-    this.$errors = {};
-    return this;
-  };
+  return fields.map(function (field) {
+    return check.call(vm, field, vm._data[field]);
+  }).indexOf(false) === -1;
+}
 
-  function vuerifyInit (_Vue, _opts) {
-    Vue$1 = _Vue;
-    Vuerify.prototype.$rules = Object.assign({}, RULES, _opts);
-    Vue$1.util.defineReactive(Vuerify.prototype, '$errors', {});
-    Vue$1.util.defineReactive(Vuerify.prototype, 'invalid', true);
-    Vue$1.util.defineReactive(Vuerify.prototype, 'valid', false);
-    Vue$1.mixin({ created: init });
-  }
+var Vuerify = function Vuerify(_vm) {
+  this.vm = _vm;
+};
 
-  function init() {
-    var _this = this;
+Vuerify.prototype.check = function (fields) {
+  return checkAll.call(this, fields);
+};
 
-    var fields = this.$options.vuerify;
+Vuerify.prototype.clear = function () {
+  this.$errors = {};
+  return this;
+};
 
-    /* istanbul ignore next */
-    if (!fields) return;
+function vuerifyInit (_Vue, _opts) {
+  Vue$1 = _Vue;
+  Vuerify.prototype.$rules = Object.assign({}, RULES, _opts);
+  Vue$1.util.defineReactive(Vuerify.prototype, '$errors', {});
+  Vue$1.util.defineReactive(Vuerify.prototype, 'invalid', true);
+  Vue$1.util.defineReactive(Vuerify.prototype, 'valid', false);
+  Vue$1.mixin({ created: init });
+}
 
-    this.$vuerify = new Vuerify(this);
-    Object.keys(fields).forEach(function (field) {
-      return _this.$watch(field, function (value) {
-        return check.call(_this, field, value);
-      });
-    });
-  }
+function init() {
+  var _this = this;
 
-  var Vue = void 0;
-  function install(_Vue, opts) {
-    /* istanbul ignore next */
-    if (Vue) {
-      console.warn('[Vuerify] already installed. Vue.use(Vuerify) should be called only once.');
-      return;
-    }
-
-    Vue = _Vue;
-    vuerifyInit(Vue, opts);
-  }
+  var fields = this.$options.vuerify;
 
   /* istanbul ignore next */
-  // auto install in dist mode
-  if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
+  if (!fields) return;
+
+  this.$vuerify = new Vuerify(this);
+  Object.keys(fields).forEach(function (field) {
+    return _this.$watch(field, function (value) {
+      return check.call(_this, field, value);
+    });
+  });
+}
+
+/* polyfill Object.assign */
+if (typeof Object.assign !== 'function') {
+  Object.assign = function (target) {
+    'use strict';
+
+    if (target == null) {
+      throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    target = Object(target);
+    for (var index = 1; index < arguments.length; index++) {
+      var source = arguments[index];
+      if (source != null) {
+        for (var key in source) {
+          if (Object.prototype.hasOwnProperty.call(source, key)) {
+            target[key] = source[key];
+          }
+        }
+      }
+    }
+    return target;
+  };
+}
+
+var Vue = void 0;
+function install(_Vue, opts) {
+  /* istanbul ignore next */
+  if (Vue) {
+    console.warn('[Vuerify] already installed. Vue.use(Vuerify) should be called only once.');
+    return;
   }
 
-  return install;
+  Vue = _Vue;
+  vuerifyInit(Vue, opts);
+}
 
-}));
+/* istanbul ignore next */
+// auto install in dist mode
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+return install;
+
+})));
